c1eaab8ca81f113bca8ee7b1e5229a5cc5ceb155
Kishore <t2657953@gmail.com>
2026-06-25 22:42:36 +0530

    Update first_non_repeat.js

--- first_non_repeat.js
+++ first_non_repeat.js
@@ -19,3 +19,75 @@ for(let i=name.length-1;i>=0;i--){
     }
 
 }
+
+//2.Last non repeative word
+
+let name = "kishore";
+let found = false;
+
+for (let i = name.length - 1; i >= 0; i--) {
+    let count = 0;
+
+    for (let j = 0; j < name.length; j++) {
+        if (name[i] === name[j] && i !== j) {
+            count++;
+        }
+    }
+
+    if (count === 0) {
+        console.log("Last", name[i]);
+        found = true;
+        break; // stop at last non-repetitive
+    }
+}
+
+if (!found) {
+    console.log("No non-repetitive character found");
+}
+//3.Last repeat word
+let name = "kishore";
+let found = false;
+
+for (let i = name.length - 1; i >= 0; i--) {
+    let count = 0;
+
+    for (let j = 0; j < name.length; j++) {
+        if (name[i] === name[j] && i !== j) {
+            count++;
+        }
+    }
+
+    if (count > 0) {  // character repeats
+        console.log("Last repetitive:", name[i]);
+        found = true;
+        break; // stop at the last repetitive character
+    }
+}
+
+if (!found) {
+    console.log("No repetitive character found");
+}
+//4.First repeat word
+    
+let name = "kishore";
+let found = false;
+
+for (let i = 0; i < name.length; i++) {
+    let count = 0;
+
+    for (let j = 0; j < name.length; j++) {
+        if (name[i] === name[j] && i !== j) {
+            count++;
+        }
+    }
+
+    if (count > 0) {  // character repeats
+        console.log("First repetitive:", name[i]);
+        found = true;
+        break; // stop at the first repetitive character
+    }
+}
+
+if (!found) {
+    console.log("No repetitive character found");
+}
